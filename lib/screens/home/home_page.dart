29bb0c952a864e61503b97417e570b20c190ee2a
Muhammad Haseeb <bang526726@gmail.com>
2024-10-04 00:45:46 +0500

    All UI part done

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_gemini_clone/screens/home/widgets/container.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,7 +9,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  //dotenv.env['API_KEY'];
+  TextEditingController textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +19,106 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _buildUI(BuildContext context) {
-    return SizedBox();
+    return Column(
+      children: [
+        const SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Container(
+            height: 70,
+            width: double.maxFinite,
+            child: Image.network(
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Google_Gemini_logo.svg/2560px-Google_Gemini_logo.svg.png",
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+        // _chatAre(context),
+        Expanded(
+          child: Container(
+            width: double.maxFinite,
+            color: Colors.grey.shade900,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 12,
+              vertical: 15,
+            ),
+            child: Stack(
+              children: [
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Hello,",
+                      style: TextStyle(
+                        fontSize: 60,
+                      ),
+                    ),
+                    Text(
+                      "How can I help you today ?",
+                      style: TextStyle(
+                        fontSize: 40,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    ContaninerWidget(
+                        innerText:
+                            "What are tips to improve public speaking skills?"),
+                    ContaninerWidget(
+                        innerText:
+                            "Find hotels in Phuket for a week, and suggest a packing list"),
+                    ContaninerWidget(
+                        innerText:
+                            "Improve the readability of the following code"),
+                    ContaninerWidget(
+                        innerText: "Find the best running trails nearby"),
+                  ],
+                ),
+                Positioned(
+                  bottom: 1,
+                  right: 0,
+                  left: 0,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: textEditingController,
+                          decoration: InputDecoration(
+                            hintText: 'Type a message...',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          color: Colors
+                              .blue, // Add a background color for visibility
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Center(
+                          child: Icon(
+                            Icons.send,
+                            color: Colors.white,
+                            size: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
---
+++ lib/screens/home/widgets/container.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ContaninerWidget extends StatelessWidget {
+  final String innerText;
+
+  const ContaninerWidget({
+    super.key,
+    required this.innerText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.grey.shade800,
+      ),
+      child: Text(
+        innerText,
+        style: const TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
